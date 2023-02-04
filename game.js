b9ae39db0d9acd61273cbfca0afa8be807e0d59a
mantasmaciulis <109831796+mantasmaciulis@users.noreply.github.com>
2023-02-04 22:15:17 +0000

    Complete Basic Funcitonality
    - Separate into clear functions
    - Add score calculation & display functionality
    - Add winner determination funcitonality
    
    - Replace for loop that adds Event Listeners into a single event listener using JS bubbling.

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
-//Nodelist of all the buttons
-const buttons = document.querySelectorAll('.choice');
-var previousChoice = null;
+const buttons = document.getElementById('moveChoices');
+const playerScoreText = document.getElementById('playerScore');
+const computerScoreText = document.getElementById('computerScore');
 
-//Setting up the event listeners
-buttons.forEach(function(button){
-    button.addEventListener('click', function(event) {
-        //Getting the value of the button
+let previousChoice = null;
+let playerScore = 0;
+let computerScore = 0;
+
+//Use JS bubbling to listen for a click on any button in movrChoices div.
+moveChoices.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'BUTTON') {
         let choice = event.target.id;
         let computerSelection = getComputerMove();
-        //Calling the playRound function
         playRound(choice, computerSelection);
-    });
+    }
 });
 
 function getComputerMove(){
-    //Randomly generate a number between 0 and 2 inclusive
     let randomNumber = Math.floor(Math.random() * 3);
     switch(randomNumber){
         case 0: return "rock";
@@ -23,7 +24,7 @@ function getComputerMove(){
     }
 }
 
-function playRound(playerSelection, computerSelection){
+function determineRoundWinner(playerSelection, computerSelection){
     if (computerSelection == playerSelection){
         //draw
     } else if (
@@ -31,8 +32,35 @@ function playRound(playerSelection, computerSelection){
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")){
         //player wins
+        playerScore++;
+        playerScoreText.textContent = playerScore;
         } else{
-            //computer wins
+        //computer wins
+        computerScore++;
+        computerScoreText.textContent = computerScore;
         }
+}
+
+function resetGame(){
+    prompt(announceWinner())
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+}
+
+function announceWinner(){
+    if (playerScore > computerScore){
+        alert("You win!");
+    } else{
+        alert("You lose!");
+}
+}
+
+function playRound(playerSelection, computerSelection){
+    determineRoundWinner(playerSelection, computerSelection);
+    if (playerScore >= 5 || computerScore >= 5){
+        resetGame();
+    }
     
 }
